20db11a48ad311bdef148d2eac4f7d6acd1fe87f
Terrarian <72149678+Terra-rian@users.noreply.github.com>
2022-03-15 11:00:06 -0400

    refactor(functions): removed unnecessary imports

--- src/assets/functions.ts
+++ src/assets/functions.ts
@@ -1,4 +1,4 @@
-import { MessageEmbed, ColorResolvable } from 'discord.js';
+import { MessageEmbed } from 'discord.js';
 
 import { owner_tag, owner_pfp } from './config.json';
 import { token } from './auth.json';
